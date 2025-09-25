526d6e1b7a6dc3ceae3318d3f6a04c5980eb646d
Donaldo Cruz <donaldoacm@gmail.com>
2025-09-24 21:25:39 -0400

    Enhanced UI
    - mini.indentscope for code block highlight.
    - indent-blankline.nvim for constant code block highlight
    - neo-tree icons requires nerd font. Using Jetbrains. Use this command to install `brew install font-jetbrains-mono-nerd-font`

---
+++ lua/plugins/mini-indentscope.lua
@@ -0,0 +1,23 @@
+return {
+	{
+		"nvim-mini/mini.nvim",
+		version = false,
+	},
+	{
+		"nvim-mini/mini.indentscope",
+		config = function()
+			require("mini.indentscope").setup({
+				symbol = "",
+				try_as_border = true,
+			})
+		end,
+		version = false,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {},
+	},
+}
